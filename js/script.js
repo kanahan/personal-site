9a51aba1d211d62466b08d260ea7a429382a005e
kanahan <kanahan0110@gmail.com>
2019-11-23 07:00:26 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+function setCookie(cname, cvalue, exdays) {
+  var date = new Date();
+  date.setTime(date.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + date.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+!(function (d) {
+  // cookie
+  if (getCookie('night').toLowerCase() === 'true') {
+    setCookie('night', 'false', 1);
+  } else {
+    setCookie('night', 'true', 1);
+    // set to display daytime
+    d.querySelector('body').className = 'day';
+    d.querySelector('.insta-link').style.display = 'none';
+    d.querySelector('#title').innerHTML = 'Developer';
+  };
+
+  console.log(`
+     _________
+    / ======= \\
+   / __________\\
+  | ___________ |
+  | | -       | |  looking for front-end developer?
+  | |         | |  https://dev.karson.tk
+  | |_________| |________________________
+  \\=____________/                        )
+  / """"""""""" \\                       /
+ / ::::::::::::: \\                  =D-'
+(_________________)
+  `);
+
+  d.addEventListener('DOMContentLoaded', (event) => {
+    //hide 000webhost div
+    const webhostEle = d.querySelector("div");
+    if (webhostEle != null)
+      webhostEle.lastChild.style.display = 'none';
+  })
+}(document));
