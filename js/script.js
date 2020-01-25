950afbb0ec894751a8f480d22724944a844e79f6
krsntn <kanahan0110@gmail.com>
2020-01-25 14:21:23 +0800

    remove webhost div

--- js/script.js
+++ js/script.js
@@ -1,38 +1,39 @@
 function setCookie(cname, cvalue, exdays) {
-  var date = new Date();
-  date.setTime(date.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  var expires = "expires=" + date.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	var date = new Date();
+	date.setTime(date.getTime() + exdays * 24 * 60 * 60 * 1000);
+	var expires = 'expires=' + date.toUTCString();
+	document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
 }
 
 function getCookie(cname) {
-  var name = cname + "=";
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+	var name = cname + '=';
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return '';
 }
 
-!(function (d) {
-  // cookie
-  if (getCookie('night').toLowerCase() === 'true') {
-    setCookie('night', 'false', 1);
-  } else {
-    setCookie('night', 'true', 1);
-    // set to display daytime
-    d.querySelector('body').className = 'day';
-    d.querySelector('.insta-link').style.display = 'none';
-    d.querySelector('#title').innerHTML = 'Devel<div class="div_bounce">o</div>per';
-  };
+!(function(d) {
+	// cookie
+	if (getCookie('night').toLowerCase() === 'true') {
+		setCookie('night', 'false', 1);
+	} else {
+		setCookie('night', 'true', 1);
+		// set to display daytime
+		d.querySelector('body').className = 'day';
+		d.querySelector('.insta-link').style.display = 'none';
+		d.querySelector('#title').innerHTML =
+			'Devel<div class="div_bounce">o</div>per';
+	}
 
-  console.log(`
+	console.log(`
      _________
     / ======= \\
    / __________\\
@@ -46,10 +47,9 @@ function getCookie(cname) {
 (_________________)
   `);
 
-  d.addEventListener('DOMContentLoaded', (event) => {
-    //hide 000webhost div
-    const webhostEle = d.querySelector("div");
-    if (webhostEle != null)
-      webhostEle.lastChild.style.display = 'none';
-  })
-}(document));
+	//hide 000webhost div
+	d.addEventListener('DOMContentLoaded', event => {
+		const allDiv = d.querySelectorAll('div');
+		if (allDiv != null) allDiv[allDiv.length - 1].style.display = 'none';
+	});
+})(document);
